c0363b77357342ead2147c679a63caeca09ef3c2
Yondon Fu <yondon.fu@gmail.com>
2018-09-26 09:53:59 -0400

    Print gas costs for pointDouble

--- contracts/CurveBabyJubJubHelper.sol
+++ contracts/CurveBabyJubJubHelper.sol
@@ -11,4 +11,8 @@ contract CurveBabyJubJubHelper {
     function pointMul(uint256 _x1, uint256 _y1, uint256 _d) public view returns (uint256 x2, uint256 y2) {
         (x2, y2) = CurveBabyJubJub.pointMul(_x1, _y1, _d);
     }
+
+    function pointDouble(uint256 _x1, uint256 _y1) public view returns (uint256 x2, uint256 y2) {
+        (x2, y2) = CurveBabyJubJub.pointDouble(_x1, _y1);
+    }
 }
--- test/CurveBabyJubJub.js
+++ test/CurveBabyJubJub.js
@@ -24,6 +24,28 @@ contract("CurveBabyJubJubHelper", () => {
         })
     })
 
+    describe("pointDouble", () => {
+        it("doubles a point", async () => {
+            let x1 = "0x274DBCE8D15179969BC0D49FA725BDDF9DE555E0BA6A693C6ADB52FC9EE7A82C"
+            let y1 = "0x5CE98C61B05F47FE2EAE9A542BD99F6B2E78246231640B54595FEBFD51EB853"
+
+            let g = await curve.pointDouble.estimateGas(x1, y1)
+            console.log(g)
+
+            x1 = "0xF3C160E26FC96C347DD9E705EB5A3E8D661502728609FF95B3B889296901AB5"
+            y1 = "0x9979273078B5C735585107619130E62E315C5CAFE683A064F79DFED17EB14E1"
+
+            g = await curve.pointDouble.estimateGas(x1, y1)
+            console.log(g)
+
+            x1 = "0x5234237DB48A066A4E072C8A345DE7BB0F5A1D7230B6D62BDAA3E317E56A820"
+            y1 = "0x2B970CF66A6744AACFF7C68DAB8DE8BF9F49AE8A56BF78F60E992F65708A36A3"
+
+            g = await curve.pointDouble.estimateGas(x1, y1)
+            console.log(g)
+        })
+    })
+
     describe("pointMul", () => {
         it("multiplies a point by a scalar", async () => {
             let x1 = "0x274DBCE8D15179969BC0D49FA725BDDF9DE555E0BA6A693C6ADB52FC9EE7A82C"
